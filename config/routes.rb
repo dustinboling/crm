a69526009afcf3ccf21123c9f3f4752dfbf2254e
alan <asebastian2@gmail.com>
2012-11-15 13:20:48 -0800

    Add pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Valve::Application.routes.draw do
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout" }
 
+  get "pages/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +52,7 @@ Valve::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'users#login'
+  root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
