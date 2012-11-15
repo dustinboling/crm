469561564949996e6abb13fefae1474d656cb889
alan <asebastian2@gmail.com>
2012-11-15 10:16:35 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Valve::Application.config.secret_token = 'ae25bfafe6633771f897be57edd93132239d24cfb4ada3cf1d72ffe36cce46623576a84813f12b9da777b36dbe382800c6e36d0188e55d7da43353f494bbb640'
